964f8e1486c857b4b62e634d5fdd7fd3b1dac535
Yusuf Ziya <34893284+yusufziy4@users.noreply.github.com>
2020-06-25 15:56:46 +0300

    v1.5

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,81 @@
 import React from "react"
 import "../style.css"
-import {NavLink,HashRouter} from "react-router-dom"
+import logo from "../img/logo.png"
+
+class HeaderButtons extends React.Component {
+    render() {
+        return(
+            <button className="header-button">
+                {this.props.text}
+            </button>
+        )
+    }
+}
+class ContactUs extends React.Component {
+    render() {
+        return(
+            <button className="rounded-button">İletişim</button>
+        )
+    }
+}
+
+class ChangeLanguage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            language:(navigator.language || navigator.userLanguage)
+        }
+    }
+    changeFlag(component) {
+        if (component.state.language === "tr") {
+            this.setState({
+                language:"en"
+            })
+        }
+        else {
+            this.setState({
+                language:"tr"
+            })
+        }
+    }
+    render() {
+        return(
+            <React.Fragment>
+                <div className="flag" id="flag" onMouseDown={()=>{this.changeFlag(this)}}>
+                    {(this.state.language).toUpperCase()}
+                </div>
+            </React.Fragment>
+        )
+    }
+}
 
 class Header extends React.Component {
+    componentDidMount() {
+        
+    }
     render() {
-        return (
-            <div id="header" style={{backgroundColor:this.props.bg?this.props.bg:"white"}} className="menu-header w-100">
-                <div className="margin f-700"> {this.props.text} </div>
-                <HashRouter>
-                    <NavLink to={this.props.send}>
-                        <button className="reverse"> {this.props.button_text} </button>
-                    </NavLink>
-                </HashRouter>
+        return(
+            <div className="header" id="header">
+                <div id="logo" className="logo ortala">
+                    <img style={{height:"60px"}} src={logo} alt="Logo"></img>
+                </div>
+                <div className="buttons" >
+                    <HeaderButtons text="Anasayfa"></HeaderButtons>
+                    <HeaderButtons text="Kurumsal"></HeaderButtons>
+                    <HeaderButtons text="Ürünlerimiz"></HeaderButtons>
+                    <HeaderButtons text="Projelerimiz"></HeaderButtons>
+                    <HeaderButtons text="İş Birliklerimiz"></HeaderButtons>
+                    <HeaderButtons text="Başarı Hikayemiz"></HeaderButtons>
+                    <div className="margin">
+                        <ContactUs></ContactUs>
+                    </div>
+                    <div className="margin">
+                        <ChangeLanguage></ChangeLanguage>
+                    </div>
+                </div>
             </div>
         )
     }
 }
-export {Header}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route } from "react-router-dom"
 import './index.css';
+import Home from './components/Home';
 import * as serviceWorker from './serviceWorker';
-
-import {Header} from "./Components/Header"
-import {Login} from "./Components/Login"
-import {MainMenu} from "./Components/MainMenu/MainMenu"
-import {Dashboard} from "./Components/Dashboard/Dashboard"
 
 ReactDOM.render(
   <React.StrictMode>
-    <form spellCheck={false}>
-      <HashRouter>
-      
-        <Route path="/register">
-          <Header button_text="Home Screen" text="go and look what we are making" send="/" ></Header>
-        </Route>
-
-        <Route path="/dashboard">
-          <Dashboard></Dashboard>
-        </Route>
-
-        <Route exact path="/" component={()=> <MainMenu></MainMenu> } ></Route>
-
-        <Route path="/login" component={()=> <Login></Login> }/>
-        
-      </HashRouter>
-    </form>
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -35,4 +14,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
