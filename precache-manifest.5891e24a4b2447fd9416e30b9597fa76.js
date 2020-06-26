4693089469dc3880f5847a32d6713fe1b606e635
Yusuf Ziya <34893284+yusufziy4@users.noreply.github.com>
2020-06-26 11:33:21 +0300

    build

---
+++ precache-manifest.5891e24a4b2447fd9416e30b9597fa76.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ed3ed3a8b54a3aa6e35f27bfb00243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f73a6d614df0ef0e00f",
+    "url": "/static/css/main.c833596c.chunk.css"
+  },
+  {
+    "revision": "a08451a5bbec81228707",
+    "url": "/static/js/2.07ef599a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.07ef599a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f73a6d614df0ef0e00f",
+    "url": "/static/js/main.0f3181f8.chunk.js"
+  },
+  {
+    "revision": "5483c41e5a7267c3a59e",
+    "url": "/static/js/runtime-main.defd43eb.js"
+  },
+  {
+    "revision": "0391c1d1a5e1a21918d0a50f97711734",
+    "url": "/static/media/logo.0391c1d1.png"
+  },
+  {
+    "revision": "8ee2f74cf9b1ba840a85e2f942b8977c",
+    "url": "/static/media/slider1.8ee2f74c.jpeg"
+  },
+  {
+    "revision": "19b7b6a8ac51a1fde39ed91e70170e65",
+    "url": "/static/media/slider2.19b7b6a8.jpg"
+  },
+  {
+    "revision": "3e56af86558be4645582c4f1edf1e36a",
+    "url": "/static/media/slider3.3e56af86.jpg"
+  }
+]);
