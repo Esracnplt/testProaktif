3431658253191617058c6ae78e1446dfc34a8789
Yusuf Ziya <34893284+yusufziy4@users.noreply.github.com>
2020-07-03 15:49:06 +0300

    slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,108 +1,103 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import '../style.css';
-import Sliders from "./Sliders"
-import Iletisim from "./Iletisim"
-import Header from "./Header"
+import React from "react";
+import "../style.css";
+import Sliders from "./Sliders";
+import Iletisim from "./Iletisim";
+import Header from "./Header";
 
-import CountUp from "react-countup"
+import CountUp from "react-countup";
 
 class Info1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      part1: "Olası Sorunlara",
+      part2: "öncesinde",
+      part3: "çözümler üretiyoruz",
+    };
+  }
   render() {
-    return(
+    return (
       <div className="center" id="info1">
-        {this.props.slogan}
+        {this.state.part1}
+        <div style={{ margin: 5, fontWeight: 700 }}>{this.state.part2}</div>
+        {this.state.part3}
       </div>
-    )
+    );
   }
 }
 
 class Prop extends React.Component {
   render() {
-    return(
-      <div className="info-prop" >
+    return (
+      <div className="info-prop">
         <div className="prop-element">
-          <div className="info-prop-header">
-            {this.props.text1}
-          </div>
-          <div className="prop-explanation">
-            {this.props.info1}
-          </div>
+          <div className="info-prop-header">{this.props.text1}</div>
+          <div className="prop-explanation">{this.props.info1}</div>
         </div>
         <div className="prop-element">
-        <div className="info-prop-header">
-            {this.props.text2}
-          </div>
-          <div className="prop-explanation">
-            {this.props.info2}
-          </div>
+          <div className="info-prop-header">{this.props.text2}</div>
+          <div className="prop-explanation">{this.props.info2}</div>
         </div>
         <div className="prop-element">
-        <div className="info-prop-header">
-            {this.props.text3}
-          </div>
-          <div className="prop-explanation">
-            {this.props.info3}
-          </div>
+          <div className="info-prop-header">{this.props.text3}</div>
+          <div className="prop-explanation">{this.props.info3}</div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 class Info2 extends React.Component {
   render() {
-    return(
+    return (
       <div className="center" id="info2">
-        <div className="info-header">
-          {this.props.header}
-        </div>
+        <div className="info-header">{this.props.header}</div>
         <div className="info-props">
-          <Prop text1="Sosyal Sorumluluk Projeleri" text2="Dış Ticaret Yönetim Sistemi" text3="DontBeCloser"
-                info1="Var Gücümüzle 'Eğitim' diyoruz." info2="Yer ve Zamanından Bağımsız, İthalat, İhracat, Antrepo,Transit, Özet Beyan ve NCTS gibi tüm işlemleri Hızlıca gerçekleştirin"
-                info3="DBC ile kendinizi güvene alın."
+          <Prop
+            text1="Sosyal Sorumluluk Projeleri"
+            text2="Dış Ticaret Yönetim Sistemi"
+            text3="DontBeCloser"
+            info1="Var Gücümüzle 'Eğitim' diyoruz."
+            info2="Yer ve Zamanından Bağımsız, İthalat, İhracat, Antrepo,Transit, Özet Beyan ve NCTS gibi tüm işlemleri Hızlıca gerçekleştirin"
+            info3="DBC ile kendinizi güvene alın."
           ></Prop>
         </div>
       </div>
-    )
+    );
   }
 }
 
 class Bulten extends React.Component {
   render() {
-    return(
+    return (
       <React.Fragment>
         <div className="bulten-cont center" id="bulten-cont">
-          <div className="bulten-header">
-            Bültene kayıt Olun
-          </div>
+          <div className="bulten-header">Bültene kayıt Olun</div>
           <div>
             <input placeholder="E-Posta Adresiniz" className="main-input" />
             <button className="main-button">Kayıt Ol</button>
           </div>
           <div className="bulten-teknik-destek">
-            Teknik Destek İçin: 
+            Teknik Destek İçin:
             {" " + this.props.telno}
           </div>
         </div>
       </React.Fragment>
-    )
+    );
   }
 }
 
 class Statistic extends React.Component {
   render() {
-    return(
+    return (
       <div className="statistic">
         <div className="statistic-count">
-          <CountUp start={0} end={this.props.count} duration={5} />
-          +
+          <CountUp start={0} end={this.props.count} duration={5} />+
         </div>
-        <div className="statistic-text">
-          {this.props.text}
-        </div>
+        <div className="statistic-text">{this.props.text}</div>
       </div>
-    )
+    );
   }
 }
 
@@ -110,42 +105,49 @@ class Istatistikler extends React.Component {
   render() {
     var Statistics = [
       [
-        {text:"Yıllık Deneyim",count:26},
-        {text:"Beyanname",count:17000000}
+        { text: "Yıllık Deneyim", count: 26 },
+        { text: "Beyanname", count: 17000000 },
       ],
       [
-        {text:"Kullanıcı",count:7800},
-        {text:"Hizmet",count:16000}
-      ]
-    ]
-    var MappedComponents = Statistics.map((object) => 
-      <div key={(object[0].text + ":" + object[1].text)} className="statistic-div">
-        <Statistic key={object[0].text} text={object[0].text} count={object[0].count} />
-        <Statistic key={object[1].text} text={object[1].text} count={object[1].count} />
+        { text: "Kullanıcı", count: 7800 },
+        { text: "Öğrenciye Hizmet", count: 16000 },
+      ],
+    ];
+    var MappedComponents = Statistics.map((object) => (
+      <div
+        key={object[0].text + ":" + object[1].text}
+        className="statistic-div"
+      >
+        <Statistic
+          key={object[0].text}
+          text={object[0].text}
+          count={object[0].count}
+        />
+        <Statistic
+          key={object[1].text}
+          text={object[1].text}
+          count={object[1].count}
+        />
       </div>
-    )
-    return(
-      <div className="statistics">
-        {MappedComponents}
-      </div>
-    )
+    ));
+    return <div className="statistics">{MappedComponents}</div>;
   }
 }
 
 class Home extends React.Component {
   render() {
-    return(
+    return (
       <React.Fragment>
         <Header />
         <div className="body">
           <Sliders></Sliders>
-          <Info1 slogan="Olası sorunlara, öncesinde çözümler üretiyoruz."></Info1>
+          <Info1></Info1>
           <Info2 header="Nitelikli yazılım, yönetim ve bilişim çözümlerimiz ile üretken karlı ve sürekli bir gelişim için proaktif vizyon"></Info2>
           <Istatistikler />
           <Iletisim></Iletisim>
         </div>
       </React.Fragment>
-    )
+    );
   }
 }
 
--- src/components/Sliders.js
+++ src/components/Sliders.js
@@ -1,202 +1,210 @@
 /* eslint-disable no-unused-vars */
-import React from "react"
-import slider1img from "../img/slider1.jpeg"
-import slider2img from "../img/slider2.jpg"
-import slider3img from "../img/slider3.jpg"
+import React from "react";
+import slider1img from "../img/slider1.jpeg";
+import slider2img from "../img/slider2.jpg";
+import slider3img from "../img/slider3.jpg";
 
-import EventBus from "js-event-bus"
+import EventBus from "js-event-bus";
 
-const MyEventBus = new EventBus()
+const MyEventBus = new EventBus();
 
-var sliderCount = 3
-var intervalHizi = 10000
+var sliderCount = 4;
+var intervalHizi = 10000;
 
-var index = 1
+var index = 1;
 function arttır(e) {
-    if (e) {
-        clearInterval(interval)
-        interval = setInterval(arttır,intervalHizi)
-    }
-    index = (index === 3)?1:index+1
-    MyEventBus.emit("currentslide",null,index)
+  if (e) {
+    clearInterval(interval);
+    interval = setInterval(arttır, intervalHizi);
+  }
+  index = index === sliderCount ? 1 : index + 1;
+  MyEventBus.emit("currentslide", null, index);
 }
 function eksilt(e) {
-    if (e) {
-        clearInterval(interval)
-        interval = setInterval(arttır,intervalHizi)
-    }
-    index = (index === 1)?sliderCount:index-1
-    MyEventBus.emit("currentslide",null,index)
+  if (e) {
+    clearInterval(interval);
+    interval = setInterval(arttır, intervalHizi);
+  }
+  index = index === 1 ? sliderCount : index - 1;
+  MyEventBus.emit("currentslide", null, index);
 }
-let interval = setInterval(arttır,intervalHizi)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let interval = setInterval(arttır, intervalHizi);
 
 class Slider extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {render:true}
+  constructor(props) {
+    super(props);
+    this.state = { render: true };
+  }
+  componentDidMount() {
+    var slider = this;
+    MyEventBus.on("currentslide", function (mesaj) {
+      if (mesaj === 1) {
+        slider.setState({ render: true });
+      } else {
+        slider.setState({ render: false });
+      }
+    });
+  }
+  componentWillUnmount() {
+    MyEventBus.detachAll();
+  }
+  render() {
+    if (this.state.render) {
+      return (
+        <React.Fragment>
+          <img
+            src={slider1img}
+            style={{ width: "100%" }}
+            alt="slider"
+            className="blur slider-img"
+          ></img>
+        </React.Fragment>
+      );
+    } else {
+      return true;
     }
-    componentDidMount() {
-        var slider = this
-        MyEventBus.on("currentslide",function(mesaj){
-            if (mesaj === 1) {
-                slider.setState({render:true})
-            }
-            else {
-                slider.setState({render:false})
-            }
-        })
-    }
-    componentWillUnmount() {
-        MyEventBus.detachAll()
-    }
-    render() {
-        if (this.state.render) {
-            return(
-                <React.Fragment>
-                    <img src={slider1img} style={{width:"100%"}} alt="slider" className="blur slider-img">
-                        
-                    </img>
-                </React.Fragment>
-            )
-        }
-        else {
-            return true
-        }
-    }
+  }
 }
 
 class Slider2 extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {render:false}
+  constructor(props) {
+    super(props);
+    this.state = { render: false };
+  }
+  componentDidMount() {
+    var slider = this;
+    MyEventBus.on("currentslide", function (mesaj) {
+      if (mesaj === 2) {
+        slider.setState({ render: true });
+      } else {
+        slider.setState({ render: false });
+      }
+    });
+  }
+  render() {
+    if (this.state.render) {
+      return (
+        <React.Fragment>
+          <img
+            src={slider2img}
+            style={{ width: "100%" }}
+            alt="slider"
+            className="blur slider-img"
+          ></img>
+        </React.Fragment>
+      );
+    } else {
+      return false;
     }
-    componentDidMount() {
-        var slider = this
-        MyEventBus.on("currentslide",function(mesaj){
-            if (mesaj === 2) {
-                slider.setState({render:true})
-            }
-            else {
-                slider.setState({render:false})
-            }
-        })
-    }
-    render(){
-        if (this.state.render) {
-            return (
-                <React.Fragment>
-                    <img src={slider2img} style={{width:"100%"}} alt="slider" className="blur slider-img">
-                        
-                    </img>
-                </React.Fragment>
-            )
-        }
-        else {
-            return false
-        }
-    }
+  }
 }
 
 class Slider3 extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {render:false}
+  constructor(props) {
+    super(props);
+    this.state = { render: false };
+  }
+  componentDidMount() {
+    var slider = this;
+    MyEventBus.on("currentslide", function (mesaj) {
+      if (mesaj === 3) {
+        slider.setState({ render: true });
+      } else {
+        slider.setState({ render: false });
+      }
+    });
+  }
+  render() {
+    if (this.state.render) {
+      return (
+        <React.Fragment>
+          <img
+            src={slider3img}
+            style={{ width: "100%" }}
+            alt="slider"
+            className="blur slider-img"
+          ></img>
+        </React.Fragment>
+      );
+    } else {
+      return false;
     }
-    componentDidMount() {
-        var slider = this
-        MyEventBus.on("currentslide",function(mesaj){
-            if (mesaj === 3) {
-                slider.setState({render:true})
-            }
-            else {
-                slider.setState({render:false})
-            }
-        })
+  }
+}
+
+class Slider4 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { render: false };
+  }
+  componentDidMount() {
+    var slider = this;
+    MyEventBus.on("currentslide", function (mesaj) {
+      if (mesaj === 4) {
+        slider.setState({ render: true });
+      } else {
+        slider.setState({ render: false });
+      }
+    });
+  }
+  render() {
+    if (this.state.render) {
+      return (
+        <React.Fragment>
+          <img
+            src={require("./../img/slider4.jpg")}
+            style={{ width: "100%" }}
+            alt="slider"
+            className="blur slider-img"
+          ></img>
+        </React.Fragment>
+      );
+    } else {
+      return false;
     }
-    render(){
-        if (this.state.render) {
-            return (
-                <React.Fragment>
-                    <img src={slider3img} style={{width:"100%"}} alt="slider" className="blur slider-img">
-                        
-                    </img>
-                </React.Fragment>
-            )
-        }
-        else {
-            return false
-        }
-    }
+  }
 }
 
 class ButtonArrow extends React.Component {
-    componentDidMount() {
-        let button = this
-        document.getElementsByClassName("arrow-" + this.props.side)[0].onmousedown = function() {
-            if (button.props.side === "left") {
-                eksilt("a")
-            }
-            else {
-                arttır("a")
-            }
+  componentDidMount() {
+    let button = this;
+    document.getElementsByClassName(
+      "arrow-" + this.props.side
+    )[0].onmousedown = function () {
+      if (button.props.side === "left") {
+        eksilt("a");
+      } else {
+        arttır("a");
+      }
+    };
+  }
+  render() {
+    return (
+      <div
+        className={
+          (this.props.side === "left" ? "arrow-left" : "arrow-right") +
+          " arrow-button center"
         }
-    }
-    render() {
-        return(
-            <div className={((this.props.side === "left")?"arrow-left":"arrow-right") + " arrow-button center"}>
-                {
-                    (this.props.side === "left")?"<":">"
-                }
-            </div>
-        )
-    }
+      >
+        {this.props.side === "left" ? "<" : ">"}
+      </div>
+    );
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 class Sliders extends React.Component {
-    render() {
-        return (
-            <div className="slider-cont">
-                <ButtonArrow side="left"></ButtonArrow>
-                <ButtonArrow side="right"></ButtonArrow>
-                <Slider></Slider>
-                <Slider2></Slider2>
-                <Slider3></Slider3>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="slider-cont">
+        <ButtonArrow side="left"></ButtonArrow>
+        <ButtonArrow side="right"></ButtonArrow>
+        <Slider></Slider>
+        <Slider2></Slider2>
+        <Slider3></Slider3>
+        <Slider4></Slider4>
+      </div>
+    );
+  }
 }
 
-export default Sliders
+export default Sliders;
