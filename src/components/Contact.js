f7d76f62e7948f0a7c9d0f410b27b8e259041f63
Yusuf Ziya <34893284+yusufziy4@users.noreply.github.com>
2020-10-19 14:28:09 +0300

    contact-map

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -68,15 +68,16 @@ class Contact extends React.Component {
     return (
       <div className="contact-cont">
         <div>
-          <iframe
+          {/*<iframe
             title="Map"
             frameBorder="0"
             width={this.state.width}
             height="300"
             style={{ border: "0px" }}
-            src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJk6ZsfABEyxQRAiKa5sxMCyo&key=AIzaSyDG3pFZMfJt4GeBlUOCK6kMNfJdlKZdggk"
+            src="https://www.google.com/maps/https://www.google.com/maps/embed/v1/place?q=Proaktif%20Dijital%20Y%C3%B6netim%20ve%20E%C4%9Fitim%20Sistemleri%20Ltd.%20%C5%9Eti.&key=AIzaSyDj7NqozXRR1OB7znxR85Qp6OvXpKyOmbw/v1/place?q=place_id:ChIJk6ZsfABEyxQRAiKa5sxMCyo&key=AIzaSyDG3pFZMfJt4GeBlUOCK6kMNfJdlKZdggk"
             allowFullScreen
-          ></iframe>
+          ></iframe>*/}
+          <iframe title="Map" width={this.state.width} height="300" frameborder="0" style={{"border":"0px"}} src="https://www.google.com/maps/embed/v1/place?q=Proaktif%20Dijital%20Y%C3%B6netim%20ve%20E%C4%9Fitim%20Sistemleri%20Ltd.%20%C5%9Eti.&key=AIzaSyDj7NqozXRR1OB7znxR85Qp6OvXpKyOmbw" allowFullScreen></iframe>
         </div>
         <div className="adresler">
           <FontAwesomeIcon icon={faMapMarkerAlt} size="2x" />
