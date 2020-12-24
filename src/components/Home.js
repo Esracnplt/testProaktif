6160d128661c6fe38df7d9586cbc19e9adea7e71
nabi <nabiguzel@gmail.com>
2020-12-24 17:27:56 +0300

    Okullar denemesi

--- src/components/Home.js
+++ src/components/Home.js
@@ -238,7 +238,14 @@ const Buttons = () => {
       html: (!hasLoadedSchools) ? <div>Bilgiler getiriliyor...<br />Lütfen bekleyiniz.</div> : <ProaktifTab tabs={egitimDestekleriTabs} />,
       onOpen: () => {
         if (!hasLoadedSchools) {
-          fetch("/index/okullar/")
+          fetch("/index/okullar/",
+          {
+            headers: {
+              //'Content-Type': 'application/x-www-form-urlencoded',
+              'Access-Control-Allow-Origin': '*',
+              'Accept': 'application/json',
+            }
+          })
             .then(response => response.json())
             .then((data) => {
               setEgitimDestekleriTabs(tabs =>
